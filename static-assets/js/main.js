8f54191dd19e37e0350266886332751e84de11c8
yangjiao2 <yjiao7@uci.edu>
2017-03-05 22:56:18 -0800

    delete page added

--- static-assets/js/main.js
+++ static-assets/js/main.js
@@ -2,6 +2,25 @@
 
 $(function() {
 
+  $(document).mouseup(function (e) {
+    var container = $(".Form");
+    var background = $(".intro-header");
+    //
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0 // ... nor a descendant of the container
+        && container.is(":visible")
+        && background.is(e.target)
+    )
+    {
+      window.location = "home";
+      container.fadeOut();
+    }
+  });
+
+  $('.form-close').click(function (){
+    var container = $(".Form");
+    container.fadeOut();
+  });
   // contact form animations
   $('#give-button').click(function() {
     $('#giveForm').fadeIn();
@@ -9,37 +28,38 @@ $(function() {
   $('#get-button').click(function() {
     $('#getForm').fadeIn();
   });
-  $(document).mouseup(function (e) {
-    var container = $(".Form");
-    //
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0 // ... nor a descendant of the container
-        && ($('.Form').is(":visible")))
 
-    {
-      window.location = "home";
-      container.fadeOut();
-    }
+
+  $('.InterestFormPopUp').click(function(){
+    var itemId = $(this).data('id');
+    $("#user_item_id").val( itemId );
+    $('#InterestFormPopUp').modal('show');
   });
 
+
+
+  // $('#interest-submit-btn').click(function(){
+  //   $('.interestForm').html('<p>deleted</p>');
+  // });
 });
 
+
 /*
-//@ sourceMappingURL=main.map
-*/
+ //@ sourceMappingURL=main.map
+ */
 
 // post view
 
 
-$(document).ready(function() {
-    $('#list').click(function(event){
-      event.preventDefault();
-      $('#posts .item').addClass('list-group-item');
-    });
-
-    $('#grid').click(function(event){
-      event.preventDefault();
-      $('#posts .item').removeClass('list-group-item');
-      $('#posts .item').addClass('grid-group-item');
-    });
-});
+// $(document).ready(function() {
+//     $('#list').click(function(event){
+//       event.preventDefault();
+//       $('#posts .item').addClass('list-group-item');
+//     });
+//
+//     $('#grid').click(function(event){
+//       event.preventDefault();
+//       $('#posts .item').removeClass('list-group-item');
+//       $('#posts .item').addClass('grid-group-item');
+//     });
+// });
