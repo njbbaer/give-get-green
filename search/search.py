a5be830e5e867ff1286f9e71472d47e7b884172e
shruti19 <skhuran1@uci.edu>
2017-03-12 03:10:39 -0700

    database creation + index creation + search + filter by zipcode completed

--- search/search.py
+++ search/search.py
@@ -8,7 +8,11 @@ import shutil
 from whoosh.index import open_dir
 from whoosh.query import *
 from whoosh.qparser import MultifieldParser
-
+from geopy.geocoders import Nominatim
+from geopy.distance import vincenty
+from geopy.distance import great_circle
+from stemming.porter2 import stem
+from nltk import PorterStemmer
 
 # CREATING A DATABASE
 def create_database():
@@ -17,23 +21,24 @@ def create_database():
 	if not os.path.exists("givegetgreen_db"):
 		conn=sqlite3.connect('givegetgreen_db');
 		c=conn.cursor();
-		c.execute('''CREATE TABLE posting_posting (title text, description text, category text, name text, zipcode text, phone text, email text)''');
-		c.execute('''INSERT INTO posting_posting VALUES ('mobile','in very good condition','electronics','a','92612','9495995959','a@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('tv','10 years old','electronics','b','92613','9495995950','b@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('radio','latest latest 1987 classic collection','electronics','a','92614','9495995951','c@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('bottle','plastic bottles 12','recyclable','b','92615','9495995952','d@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('jeans','blue denim latest style','clothes','a','92616','9495995953','e@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('chair','wooden blue wooden blue 2 latest latest latest latest latest','furniture','b','92617','9495995954','f@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('pot','very large but still weightless','household','g','92623','9495995959','g@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('camera','2 years old in very good condition','electronics','h','92633','9495994550','h@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('table','plastic white new','furniture','i','92644','9495995951','i@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('matress','queen size white soft latest','household','j','92655','9495954952','j@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('shirts','jean wooden blue white red gree palo','clothes','k','92666','9495235953','k@uci.edu')''');
-		c.execute('''INSERT INTO posting_posting VALUES ('pants','3 denim shades of blue latest','clothes','l','92677','9495945454','l@uci.edu')''');
-		for row in c.execute('SELECT * FROM posting_posting'):
-			print row
+		c.execute('''CREATE TABLE posting_posting (id numeric, title text, description text, category text, name text, address text, phone text, email text)''');
+		c.execute('''INSERT INTO posting_posting VALUES (1,'mobiles','in very good condition','electronics','a','3800 parkview lane irvine','9495995959','a@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (2,'tv','10 years old','electronics','b','3801 parkview lane irvine','9495995950','b@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (3,'radio','latest latest 1987 classic collection','electronics','a','university of california irvine','9495995951','c@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (4,'bottle','plastic bottles 12','recyclable','b','harvard court irvine','9495995952','d@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (5,'jeans','blue denim latest style','clothes','a','stanford court irvine','9495995953','e@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (6,'chair','wooden blue wooden blue 2 latest latest latest latest latest','furniture','b','barranca irvine','9495995954','f@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (7,'pot','very large but still weightless','household','g','2512 walnut avenue tustin','9495995959','g@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (8,'camera','2 years old in very good condition','electronics','h','3901 parkview ln','9495994550','h@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (9,'table','plastic white new','furniture','i','university center irvine','9495995951','i@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (10,'matress','queen size white soft latest','household','j','berkley avenue irvine','9495954952','j@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (11,'shirts','jean wooden blue white red green palo','clothes','k',' 18601 Airport Way, Santa Ana, CA 92707','9495235953','k@uci.edu')''');
+		c.execute('''INSERT INTO posting_posting VALUES (12,'pants','3 denim shades of blue latest','clothes','l','3601 parkview lane irvine','9495945454','l@uci.edu')''');
+		# for row in c.execute('SELECT * FROM posting_posting'):
+			# print row
 		conn.commit()
 		conn.close()
+		print "Database created"
 
 
 # CREATING AN INDEX OF DATABASE
@@ -42,39 +47,101 @@ def create_index(indexdir,database_name):
 		os.mkdir("indexdir")	
 	conn=sqlite3.connect(database_name);
 	c=conn.cursor();
-	schema = Schema(title=TEXT(stored=True),description=TEXT(stored=True),category=TEXT(stored=True),name=STORED, zipcode=STORED,phone=STORED,email=STORED)
+	schema = Schema(id=NUMERIC(stored=True),title=TEXT(stored=True),description=TEXT(stored=True),category=TEXT(stored=True),name=STORED, address=STORED,phone=STORED,email=STORED)
 	ix = create_in(indexdir, schema)
 	writer = ix.writer()
-	for row in c.execute('SELECT title,description,category,name,zipcode,phone,email FROM posting_posting'):
-		writer.add_document(title=row[0], description=row[1], category=row[2], name=row[3], zipcode=row[4], phone=row[5], email=row[6])
+	for row in c.execute('SELECT id,title,description,category,name,address,phone,email FROM posting_posting'):
+		d1=" "
+		d2=" "
+		d3=" "
+		x1=row[1].split(" ")
+		for words in x1:
+			d1=d1 + PorterStemmer().stem_word(words) + " "
+		x2=row[2].split(" ")
+		for words in x2:
+			d2=d2 + PorterStemmer().stem_word(words) + " "
+		x3=row[3].split(" ")
+		for words in x3:
+			d3=d3 + PorterStemmer().stem_word(words) + " "
+		writer.add_document(id=row[0],title=d1, description=d2, category=d3, name=(row[4].lower()), address=(row[5].lower()), phone=(row[6].lower()), email=(row[7].lower()))
 		# print row
 	writer.commit()
+	print "index created"
 
 
 # PARSE A QUERY STRING AND SEARCH RESULTS IN SORTED ORDER
-def query_result(search_query):
+def query_result(getter_address, search_query):
+
+	database_name="givegetgreen_db"
+	conn=sqlite3.connect(database_name);
+
+	address_list=[]
+	all_fields_list=[]
 	hits=[]
 	d={}
 	ix=open_dir("indexdir")
 	f=open("search_results.txt","a")
+
 	query=""
 	search_list= search_query.split(" ")
 	for words in search_list:
+		words=PorterStemmer().stem_word(words.lower())
 		query= str(query) + str(words) + " OR "
 	query=query+" "
 	search_query=query
-	print search_query		
+	# print search_query		
 	with ix.searcher() as searcher:
-		query = MultifieldParser(["title", "description"], schema=ix.schema).parse(search_query)
+		query = MultifieldParser(["title","category","description"], schema=ix.schema).parse(search_query)
     	# query = QueryParser("description" AND "title", ix.schema).parse(u'latest')
 		results = searcher.search(query)
-		# print results[0]
+		# print results
 		for words in results:
 			f.write(str(words)+"\n")
 			hits.append(str(words))
-			print words.score, words
-	return hits		
+			# print words.score, words['address']
+			address_list.append(words['address'])
 
+			c=conn.cursor();
+			x=int(words['id'])
+			x=(x,)
+			# print x
+			for row in c.execute('SELECT * FROM posting_posting where id= ? ',x ):
+				all_fields_list.append(row);
+	getter_address= (getter_address.lower())
+	conn.commit()
+	conn.close()
+	return add_filter(getter_address, address_list, all_fields_list)
+
+
+# FILTER THE RESULTS ACOORDING TO THE ADDRESS
+def add_filter(getter_address, address_list, all_fields_list):
+	distances=[]
+	final_list=[]
+	final_all_fields_list=[]
+	geolocator = Nominatim()		
+	location1 = geolocator.geocode(str(getter_address))
+	if len(location1)==0:
+		pass
+	location1_ll = (location1.latitude, location1.longitude)
+	# print len(address_list)
+	for i in range(0,len(address_list)):
+		# print words
+		try:
+			words=address_list[i]
+			# print (words)
+			location2 = geolocator.geocode(str(words))
+			location2_ll = (location2.latitude, location2.longitude)
+			# print "in"
+			distance=(vincenty(location1_ll, location2_ll).miles)
+			if distance<=25:
+				# print words, distance
+				final_list.append(words)
+				final_all_fields_list.append(all_fields_list[i])
+		except Exception as e :
+			print e
+			continue
+	return final_all_fields_list
+			
 
 # MAIN FUNCTION
 if __name__=="__main__":
@@ -87,7 +154,10 @@ if __name__=="__main__":
 	create_index(indexdir,database_name)
 
 	# assign search_query as entered by the user 
-	# hits contains the top search results sorted
-	search_query='jean wooden'
-	hits=query_result(search_query)
-	print hits
+	# hits contains the top search results sorted by relevance and filtered by zipcode
+	# hit is the tuple.. use this to display results on web page
+	search_query='latest'
+	getter_address="3801 parkview lane irvine"
+	hits=query_result(getter_address, search_query)
+	for hit in hits:
+		print (hit)
