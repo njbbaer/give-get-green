54c7edc79783c97b5e1844fc56d1185e70a9c86d
Nathaniel Baer <njbbaer@gmail.com>
2017-03-11 16:23:35 -0800

    Update database post

--- givegetgreen/addPost/views.py
+++ givegetgreen/addPost/views.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python2.7
 from django.views.generic import TemplateView
-from givegetgreen.posting.models import Posting
 
 class AddPostView(TemplateView):
     template_name = 'viewpost.html'
@@ -11,40 +10,6 @@ class AddPostView(TemplateView):
         print postdict
         context = ''
         
-	## insert to database by submit giveForm
-#        if (request.method == 'POST') and ("submit-giveForm" in postdict) :
-#            user_name = request.POST.get('user_name', '')
-#            user_email = request.POST.get('user_email', '')
-#            user_zipcode = request.POST.get('user_zipcode', '')
-#            user_phone = request.POST.get('user_phone', '')
-#            user_item_category = request.POST.get('user_item_category', '')
-#            user_item = request.POST.get('user_item', '')
-#            user_item_description = request.POST.get('user_item_description', '')
-#            user_item_id = 1
-#
-#            posting = Posting(
-#                name = user_name,
-#                email = user_email,
-#                zipcode = user_zipcode,
-#                phone = user_phone,
-#                item = user_item_category,
-#                item_category = user_item_category,
-#                item_description = user_item_description
-#            )
-#            posting.save()
-#
-#            form_submitted = {
-#                'user_name':user_name,
-#                'user_email':user_email,
-#                'user_zipcode':user_zipcode,
-#                'user_phone':user_phone,
-#                'user_item_id':user_item_id,
-#                'user_item_category': user_item_category,
-#                'user_item':user_item,
-#                'user_item_description':user_item_description
-#            }
-#            context = {'title': 'Submitted', 'posts': [form_submitted]}
-	## search database by submitting search form
         if (request.method == 'POST') and ("submit-searchForm" in postdict) :
             context = {'title':'Search results', 'posts':[]}	
         return self.render_to_response(context)
--- givegetgreen/home/views.py
+++ givegetgreen/home/views.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python2.7
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
+from givegetgreen.posting.models import Posting
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -20,11 +21,12 @@ class HomeView(TemplateView):
         }
         postdict = dict(request.POST)
         print postdict
-	## insert to database by submit giveForm
+
+        # insert to database by submit giveForm
         if (request.method == 'POST') and ("submit-giveForm" in postdict) :
             user_name = request.POST.get('user_name', '')
             user_email = request.POST.get('user_email', '')
-            user_zipcode = request.POST.get('user_zipcode', '')
+            user_address = request.POST.get('user_zipcode', '')
             user_phone = request.POST.get('user_phone', '')
             user_item_category = request.POST.get('user_item_category', '')
             user_item = request.POST.get('user_item', '')
@@ -34,11 +36,11 @@ class HomeView(TemplateView):
             posting = Posting(
                 name = user_name,
                 email = user_email,
-                zipcode = user_zipcode,
+                address = user_address,
                 phone = user_phone,
-                item = user_item_category,
-                item_category = user_item_category,
-                item_description = user_item_description
+                title = user_item_category,
+                category = user_item_category,
+                description = user_item_description
             )
             posting.save()
 
