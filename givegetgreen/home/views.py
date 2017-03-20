10adf6008d7d50b05d0a78a56cbba22ee543924d
Yang <yjiao7@uci.edu>
2017-03-20 12:53:56 -0700

    bug fix

--- givegetgreen/home/views.py
+++ givegetgreen/home/views.py
@@ -6,14 +6,14 @@ from search import search
 
 class HomeView(TemplateView):
     template_name = 'home.html'
-
+    
     def get(self, request, *args, **kwargs):
         context = {
             'title': 'Give Get Green',
             'some_dynamic_value': 'This text comes from django view!',
         }
         return self.render_to_response(context)
-
+    
     def post(self, request, *args, **kwargs):
         print "put"
         context = {
@@ -22,7 +22,7 @@ class HomeView(TemplateView):
         }
         postdict = dict(request.POST)
         print postdict
-
+        
         # insert to database by submit giveForm
         if (request.method == 'POST') and ("submit-giveForm" in postdict) :
             user_name = request.POST.get('user_name', '')
@@ -33,18 +33,18 @@ class HomeView(TemplateView):
             user_item = request.POST.get('user_item', '')
             user_item_description = request.POST.get('user_item_description', '')
             user_item_id = 1
-
+            
             posting = Posting(
-                name = user_name,
-                email = user_email,
-                address = user_address,
-                phone = user_phone,
-                title = user_item_category,
-                category = user_item_category,
-                description = user_item_description
-            )
-            posting.save()
-
-            search.create_index("indexdir", "givegetgreen_db")
-
+                              name = user_name,
+                              email = user_email,
+                              address = user_address,
+                              phone = user_phone,
+                              title = user_item_category,
+                              category = user_item_category,
+                              description = user_item_description
+                              )
+                              posting.save()
+                              
+                              search.create_index("indexdir", "givegetgreen_db")
+                              
             return redirect('/home', context);
